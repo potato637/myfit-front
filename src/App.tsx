aa06de2347bdf0bd51d4b1c98b0a708743f553b7
potatodev-777 <potatodev777@gmail.com>
2025-07-02 00:50:44 +0900

    modify app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import Profile from "./pages/profile/Profile";
-
 function App() {
-  return (
-    <div className="font-sans max-w-md mx-auto w-full min-h-screen">
-      <Profile />
-    </div>
-  );
+  return <div className="font-sans max-w-md mx-auto w-full min-h-screen"></div>;
 }
 
 export default App;
