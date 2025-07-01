dc085edf31725d4ce9f5aecd5340e9d918f54cb5
potatodev-777 <potatodev777@gmail.com>
2025-07-01 13:08:28 +0900

    협업 전 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="font-sans max-w-md mx-auto w-full min-h-screen">
+    <div className="font-sans max-w-sm mx-auto w-full min-h-screen bg-ct-main-blue-100">
       <Outlet />
     </div>
   );
