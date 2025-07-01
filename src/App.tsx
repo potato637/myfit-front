f8c8831af84bb7adf4b04d2bf60e589f6a4cfac1
potatodev-777 <potatodev777@gmail.com>
2025-07-02 01:12:19 +0900

    modify App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { Outlet } from "react-router-dom";
-
 function App() {
-  return (
-    <div className="font-sans max-w-md mx-auto w-full min-h-screen">
-      <Outlet />
-    </div>
-  );
+  return <div className="font-sans max-w-md mx-auto w-full min-h-screen"></div>;
 }
 
 export default App;
