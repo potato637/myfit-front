792b65dd3c5c53537b4f745b9640d35766912c09
JSC <potatodev777@gmail.com>
2025-07-01 14:56:30 +0900

    Feat/bottom nav (#6)
    * doc: add bottom nav icons svg
    
    * feat: bottom nav add svg
    
    * modify App

--- src/components/layouts/BottomNav.tsx
+++ src/components/layouts/BottomNav.tsx
@@ -1,24 +1,44 @@
 function BottomNav() {
   return (
     <div className="w-full h-[89px] fixed bottom-0 left-0 right-0 ct-center">
-      <div className="flex-1 ct-center flex-col">
-        <img src="" alt="피드" className="w-[22px] h-[22px]" />
+      <div className="flex-1 ct-center flex-col gap-1">
+        <img
+          src="/assets/bottom-nav/feed.svg"
+          alt="피드"
+          className="w-[22px] h-[22px]"
+        />
         <span className="text-body3">피드</span>
       </div>
-      <div className="flex-1 ct-center flex-col">
-        <img src="" alt="카드 검색" className="w-[22px] h-[22px]" />
+      <div className="flex-1 ct-center flex-col gap-1">
+        <img
+          src="/assets/bottom-nav/card.svg"
+          alt="카드 검색"
+          className="w-[22px] h-[22px]"
+        />
         <span className="text-body3">카드 검색</span>
       </div>
-      <div className="flex-1 ct-center flex-col">
-        <img src="" alt="채팅" className="w-[22px] h-[22px]" />
+      <div className="flex-1 ct-center flex-col gap-1">
+        <img
+          src="/assets/bottom-nav/chat.svg"
+          alt="채팅"
+          className="w-[22px] h-[22px]"
+        />
         <span className="text-body3">채팅</span>
       </div>
-      <div className="flex-1 ct-center flex-col">
-        <img src="" alt="리크루팅" className="w-[22px] h-[22px]" />
+      <div className="flex-1 ct-center flex-col gap-1">
+        <img
+          src="/assets/bottom-nav/recruiting.svg"
+          alt="리크루팅"
+          className="w-[22px] h-[22px]"
+        />
         <span className="text-body3">리크루팅</span>
       </div>
-      <div className="flex-1 ct-center flex-col">
-        <img src="" alt="마이페이지" className="w-[22px] h-[22px]" />
+      <div className="flex-1 ct-center flex-col gap-1">
+        <img
+          src="/assets/bottom-nav/profile.svg"
+          alt="마이페이지"
+          className="w-[22px] h-[22px]"
+        />
         <span className="text-body3">마이페이지</span>
       </div>
     </div>
