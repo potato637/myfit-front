d4230e925fbb9f92cbe0c6307299a61ef6d1c973
potatodev-777 <potatodev777@gmail.com>
2025-07-28 22:17:46 +0900

    query Client

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./routes/router.tsx";
 import { AppProvider } from "./contexts/appContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppProvider>
-      <RouterProvider router={router} />
-    </AppProvider>
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <RouterProvider router={router} />
+      </AppProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
