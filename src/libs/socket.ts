0827b06fad5899a9c227f6bb67314a5d1887be5b
potatodev-777 <potatodev777@gmail.com>
2025-08-11 16:46:59 +0900

    http-to-wss

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://3.36.59.150:3000", {
+const socket = io("wss://3.36.59.150:3000", {
   transports: ["websocket"],
   withCredentials: true,
 });
