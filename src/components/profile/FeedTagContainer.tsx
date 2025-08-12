344a29c86c661b01882f0bcd0ae8d534072ec1b0
JSC <potatodev777@gmail.com>
2025-08-12 15:29:54 +0900

    feat/hardcoading (#7)

--- src/components/profile/FeedTagContainer.tsx
+++ src/components/profile/FeedTagContainer.tsx
@@ -1,8 +1,7 @@
 import FeedTagItem from "./FeedTagItem";
 
-const tagList = ["광고하는사람들", "Freelancer", "DailyInsight", "AdLife"];
-
-function FeedTagContainer() {
+function FeedTagContainer({ tags }: { tags: string }) {
+  const tagList = tags.split(",");
   return (
     <div className="gap-[5px] flex flex-wrap w-full justify-start">
       {tagList.map((tag, index) => (
