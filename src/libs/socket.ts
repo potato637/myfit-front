56154cefbadd66ff2dabbe244fea4cf1bd3538f0
potatodev-777 <potatodev777@gmail.com>
2025-08-12 16:34:19 +0900

    dddd

--- src/libs/socket.ts
+++ src/libs/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("wss://3.36.59.150:3000", {
+const socket = io("https://myfit.my", {
   transports: ["websocket"],
   withCredentials: true,
 });
