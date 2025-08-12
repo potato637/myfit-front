5c379e1389b579bed3a39444eb538f92dd442d95
potatodev-777 <potatodev777@gmail.com>
2025-08-12 15:04:12 +0900

    카드 추가 안보이게

--- src/components/profile/ProfileCardContainer.tsx
+++ src/components/profile/ProfileCardContainer.tsx
@@ -2,9 +2,11 @@ import ProfileCardItem from "./ProfileCardItem";
 import { useGetCards } from "../../hooks/mypageQueries";
 import { useAuth } from "../../contexts/AuthContext";
 import ProfileAddCard from "./ProfileAddCard";
+import { useLocation } from "react-router-dom";
 
 function ProfileCardContainer({ serviceId }: { serviceId?: string }) {
   const { user } = useAuth();
+  const location = useLocation();
 
   const { data: cards, isLoading: cardsLoading } = useGetCards({
     service_id: serviceId ? serviceId : user?.id?.toString() || "",
@@ -24,7 +26,7 @@ function ProfileCardContainer({ serviceId }: { serviceId?: string }) {
           : "grid grid-cols-2 gap-3"
       }`}
     >
-      <ProfileAddCard />
+      {location.pathname.startsWith("/mypage") && <ProfileAddCard />}
       {cardsData?.map((card) => (
         <ProfileCardItem key={card.id} card={card} serviceId={serviceId} />
       ))}
