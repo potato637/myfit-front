8ceb778bf66100539fe650d066bc351044346ff6
JSC <potatodev777@gmail.com>
2025-08-12 15:09:18 +0900

    Feat/junha (#6)
    * 카드 추가 안보이게
    
    * d

--- src/components/profile/ProfileFeedContainer.tsx
+++ src/components/profile/ProfileFeedContainer.tsx
@@ -2,9 +2,11 @@ import ProfileFeedItem from "./ProfileFeedItem";
 import { useGetFeeds } from "../../hooks/mypageQueries";
 import { useAuth } from "../../contexts/AuthContext";
 import ProfileAddFeed from "./ProfileAddFeed";
+import { useLocation } from "react-router-dom";
 
 function ProfileFeedContainer({ serviceId }: { serviceId?: string }) {
   const { user } = useAuth();
+  const location = useLocation();
 
   const { data: feeds, isLoading: feedsLoading } = useGetFeeds({
     service_id: serviceId ? serviceId : user?.id?.toString() || "",
@@ -24,7 +26,7 @@ function ProfileFeedContainer({ serviceId }: { serviceId?: string }) {
           : "grid grid-cols-3 gap-1"
       }`}
     >
-      <ProfileAddFeed />
+      {location.pathname.startsWith("/mypage") && <ProfileAddFeed />}
       {feedsData?.map((feed) => (
         <ProfileFeedItem key={feed.feed_id} feed={feed} serviceId={serviceId} />
       ))}
