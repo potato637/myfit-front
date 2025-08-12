84c16a02cca781a8721023608d05dfe9fa9d9d6c
Minsu Kwon <kms37480@naver.com>
2025-08-12 15:59:21 +0900

    test44

--- src/components/onboarding/RegionModal.tsx
+++ src/components/onboarding/RegionModal.tsx
@@ -15,7 +15,7 @@ function RegionModal({ onConfirm }: { onConfirm: (val: string) => void }) {
 
   return (
     <div className="flex flex-col ct-center w-full h-full relative">
-      <div className="absolute w-full top-[146px] left-[14px] h-[36px] bg-[#EAF6FF] z-10 pointer-events-none rounded-[6px]" />
+      <div className="absolute w-full top-[146px] left-0 h-[36px] bg-[#EAF6FF] z-10 pointer-events-none rounded-[6px]" />
       <span className="text-h2 text-ct-black-200 mt-[19px]">주 활동 지역</span>
       <div className="mt-[15px] border-t border-[#A8A8A8] w-full">
         <Picker
