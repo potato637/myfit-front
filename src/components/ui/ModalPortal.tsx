6e2c73de557535c701b1f41fd3b6b7c4710dda1a
potatodev-777 <potatodev777@gmail.com>
2025-08-13 23:24:38 +0900

    asd

--- src/components/ui/ModalPortal.tsx
+++ src/components/ui/ModalPortal.tsx
@@ -1,5 +1,5 @@
 import { createPortal } from "react-dom";
-import { useRef, ReactNode, MouseEvent, useCallback } from "react";
+import { useRef, ReactNode, MouseEvent, useCallback, useEffect } from "react";
 import { useModal } from "../../contexts/ui/modalContext";
 
 interface ModalPortalProps {
@@ -21,9 +21,22 @@ const ModalPortal = ({ children, onClose }: ModalPortalProps) => {
     [onClose, closeModal]
   );
 
+  const setVh = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+
+  useEffect(() => {
+    setVh();
+    window.addEventListener("resize", setVh);
+    return () => {
+      window.removeEventListener("resize", setVh);
+    };
+  }, []);
+
   return createPortal(
     <div
-      className="fixed inset-0 w-full h-full bg-ct-black-100/50 z-[9999] ct-center"
+      className="modal-overlay bg-ct-black-100/50 z-[9999] ct-center"
       onClick={handleOutsideClick}
     >
       <div
