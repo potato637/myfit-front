9257ca3423a644a7354f9dd10c2e6b4a2149fdf5
potatodev-777 <potatodev777@gmail.com>
2025-08-14 23:38:41 +0900

    123

--- src/components/searching/SearchingSwipeItem.tsx
+++ src/components/searching/SearchingSwipeItem.tsx
@@ -4,11 +4,7 @@ function SearchingSwipeItem({ card }: { card: SectorBaseSearchingItem }) {
   return (
     <div className="w-[300px] h-[570px] flex flex-col items-center rounded-[16px] py-[16px] px-[10px] shadow-find-card">
       <div className="w-full flex flex-col gap-[8px]">
-        <img
-          src={card.card_img}
-          className="w-full h-[430px] object-cover rounded-[8px]"
-          alt="card"
-        />
+        <span className="text-h2 text-ct-black-100">{`${card.author_name} | ${card.recruiting_status}`}</span>
         <div className="w-full flex gap-[4px] justify-start items-center flex-wrap">
           {card.keywords.map((keyword, index) => (
             <div
@@ -21,6 +17,11 @@ function SearchingSwipeItem({ card }: { card: SectorBaseSearchingItem }) {
             </div>
           ))}
         </div>
+        <img
+          src={card.card_img}
+          className="w-full h-[430px] object-cover rounded-[8px]"
+          alt="card"
+        />
         <div className="w-full p-[12px] bg-ct-gray-100 flex flex-col rounded-[5px] gap-2">
           <span className="text-sub1 text-ct-black-100">한줄 소개</span>
           <span className="text-body3 text-ct-black-200">
