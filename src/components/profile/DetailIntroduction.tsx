83320acaec72bfa697a422310f05a5f4ec1ab80d
potatodev-777 <potatodev777@gmail.com>
2025-08-14 14:13:41 +0900

    asd

--- src/components/profile/DetailIntroduction.tsx
+++ src/components/profile/DetailIntroduction.tsx
@@ -19,7 +19,7 @@ function DetailIntroduction() {
 
   return (
     <>
-      <div className="w-full h-[61px] flex items-center px-[17px] gap-[7px] bg-ct-white fixed z-10 left-0 top-[calc(pb-safe+42px)]">
+      <div className="w-full h-[61px] flex items-center px-[17px] gap-[7px] bg-ct-white fixed z-10 left-0 top-42px">
         <img
           src={profile?.result.service.profile_img}
           alt="프로필 이미지"
