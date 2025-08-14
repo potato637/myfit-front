a995c4fa36da5bcc0750317fde58fd45ccf98176
potatodev-777 <potatodev777@gmail.com>
2025-08-14 15:36:47 +0900

    fdsf

--- src/components/profile/DetailIntroduction.tsx
+++ src/components/profile/DetailIntroduction.tsx
@@ -19,7 +19,7 @@ function DetailIntroduction() {
 
   return (
     <>
-      <div className="w-full h-[61px] flex items-center px-[17px] gap-[7px] bg-ct-white fixed z-10 left-0 top-[calc(42px+env(safe-area-inset-top))]">
+      <div className="w-full h-[61px] flex items-center px-[17px] gap-[7px] bg-ct-white fixed z-10 left-0 top-[calc(pb-safe+42px)]">
         <img
           src={profile?.result.service.profile_img}
           alt="프로필 이미지"
@@ -32,7 +32,7 @@ function DetailIntroduction() {
           {profile?.result.service.low_sector}
         </span>
       </div>
-      <div className="w-full h-[30px]" />
+      <div className="w-full h-[61px]" />
     </>
   );
 }
