460cf5c3fe811be6a10d1f080a9c75faff5e33bb
potatodev-777 <potatodev777@gmail.com>
2025-08-14 15:33:13 +0900

    fdssf

--- src/components/ui/ModalPortal.tsx
+++ src/components/ui/ModalPortal.tsx
@@ -11,8 +11,10 @@ const ModalPortal = ({ children, onClose }: ModalPortalProps) => {
   const modalRef = useRef<HTMLDivElement>(null);
   const { closeModal } = useModal();
 
+  // 오버레이 역할을 하는 div의 클릭 이벤트를 처리
   const handleOutsideClick = useCallback(
     (e: MouseEvent) => {
+      // 모달 내용(modalRef)을 클릭하지 않았을 때만 모달 닫기
       if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
         onClose();
         closeModal();
@@ -22,11 +24,16 @@ const ModalPortal = ({ children, onClose }: ModalPortalProps) => {
   );
 
   return createPortal(
+    // 이 div가 오버레이 역할을 함
     <div
-      className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[9999]"
+      className="fixed inset-0 z-[9999] ct-center"
       onClick={handleOutsideClick}
     >
-      <div className="w-[333px] my-8 bg-ct-white rounded-[30px]" ref={modalRef}>
+      <div
+        ref={modalRef}
+        className="w-[333px] my-8 bg-ct-white rounded-[30px]"
+        onClick={(e) => e.stopPropagation()} // 모달 내부 클릭 시 이벤트 버블링 방지
+      >
         {children}
       </div>
     </div>,
