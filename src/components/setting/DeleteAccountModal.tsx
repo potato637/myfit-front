dfdfbfae8f9f077065f47ec447b62af4b5069f79
potatodev-777 <potatodev777@gmail.com>
2025-08-14 14:07:42 +0900

    asd

--- src/components/setting/DeleteAccountModal.tsx
+++ src/components/setting/DeleteAccountModal.tsx
@@ -32,7 +32,7 @@ function DeleteAccountModal({ onDeleteSuccess }: DeleteAccountModalProps) {
               onSuccess: () => {
                 closeModal();
                 onDeleteSuccess();
-              }
+              },
             });
           }}
         >
